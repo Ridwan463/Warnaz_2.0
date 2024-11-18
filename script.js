090a3e6b05a6fec73e99bc0acd22b8230e6b71af
Ridwan463 <intisarredwan77851@gmail.com>
2024-11-18 18:46:54 +0600

    update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const productList = document.getElementById('product-list');
 
 // API URL from Sheety (update with your API URL)
-const API_URL = "https://api.sheety.co/<your-project-id>/<spreadsheet-name>/<sheet-name>";
+const API_URL = "https://api.sheety.co/a3d097db8d561a3b1cabc1e52684d8fd/warnazSpreadsheet/sheet1";
 
 // Function to fetch products from the Sheety API
 async function fetchProducts() {
